9b0d134c678ef98af77caaa449a398746cefbb3c
raf <rafs@ramtracking.com>
2025-02-12 11:59:19 +0000

    fix: added lombok to jvm

--- shell.nix
+++ shell.nix
@@ -4,22 +4,22 @@
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     # backend
-    h2
     jdk21
     maven3
+    google-java-format
     # frontend
     pnpm
     nodejs_22
   ];
 
   shellHook = ''
+    export JDTLS_JVM_ARGS="-javaagent:$HOME/.m2/repository/org/projectlombok/lombok/1.18.36/lombok-1.18.36.jar"
     echo
     echo -e "\033[1;35mwelcome to the development shell!\033[0m"
     echo "
     backend
     ├── jdk21
-    ├── maven3
-    └── h2
+    └── maven3
 
     frontend
     ├── pnpm
