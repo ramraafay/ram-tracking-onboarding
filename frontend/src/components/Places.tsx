5b06d2785b939668ce2116ee47307296372d886d
raf <rafs@ramtracking.com>
2025-02-19 20:27:37 +0000

    chore: new marker on right click

--- frontend/src/components/Places.tsx
+++ frontend/src/components/Places.tsx
@@ -8,7 +8,6 @@ import {
   Marker,
   Popup,
   TileLayer,
-  useMap,
   useMapEvents,
 } from "react-leaflet";
 import FavouritePlaces from "./FavouritePlaces";
@@ -25,16 +24,6 @@ const Places: React.FC<PlacesProps> = ({ authToken }) => {
       token: authToken,
     },
   );
-
-  const MapEvents = () => {
-    useMapEvents({
-      click(e) {
-        console.log(e.latlng.lat);
-        console.log(e.latlng.lng);
-      },
-    });
-    return false;
-  };
 
   const handleDelete = async (id?: number) => {
     try {
@@ -57,45 +46,69 @@ const Places: React.FC<PlacesProps> = ({ authToken }) => {
     }
   };
 
+  const post = async (latitude: number, longitude: number) => {
+    try {
+      const place: PlaceDTO = {
+        name: "test",
+        latitude: latitude,
+        longitude: longitude,
+      };
+      const response = await fetch("http://localhost:8080/places", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authToken}`,
+        },
+        body: JSON.stringify(place),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const responseData = await response.json();
+      console.log("Success:", responseData);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const MapEvents = () => {
+    useMapEvents({
+      contextmenu(e) {
+        console.log(e.latlng.lat);
+        console.log(e.latlng.lng);
+        post(e.latlng.lat, e.latlng.lng);
+      },
+    });
+    return false;
+  };
+
   return loading ? (
     <h1>Loading..</h1>
   ) : (
     <div className="flex flex-col gap-4">
-      <div
-        onContextMenu={(e) => {
-          e.preventDefault();
-          console.log("hello");
-        }}
-      >
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <MapEvents />
-          {data &&
-            data.map((place) => (
-              <Marker
-                key={place.id}
-                position={[place.latitude, place.longitude]}
-              >
-                <Popup>
-                  {place.name}
-                  <Button
-                    colour={ButtonColour.Red}
-                    onClick={() => handleDelete(place.id)}
-                  >
-                    Delete
-                  </Button>
-                </Popup>
-              </Marker>
-            ))}
-        </MapContainer>
-      </div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <MapEvents />
+        {data &&
+          data.map((place) => (
+            <Marker key={place.id} position={[place.latitude, place.longitude]}>
+              <Popup>
+                {place.name}
+                <Button
+                  colour={ButtonColour.Red}
+                  onClick={() => handleDelete(place.id)}
+                >
+                  Delete
+                </Button>
+              </Popup>
+            </Marker>
+          ))}
+      </MapContainer>
       <Button
         colour={ButtonColour.Purple}
         onClick={() => console.log(authToken)}
