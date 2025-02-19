8d224855a073dd4466e30c842be537751143e6c9
raf <rafs@ramtracking.com>
2025-02-19 12:16:46 +0000

    chore: places on the map

--- frontend/src/components/Places.tsx
+++ frontend/src/components/Places.tsx
@@ -28,11 +28,12 @@ const Places: React.FC<PlacesProps> = ({ authToken }) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {data &&
+          data.map((place) => (
+            <Marker position={[place.latitude, place.longitude]}>
+              <Popup>{place.name}</Popup>
+            </Marker>
+          ))}
       </MapContainer>
       <Button
         colour={ButtonColour.Purple}
