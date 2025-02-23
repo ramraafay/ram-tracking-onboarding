5973a154d024a6909bdef6091eccb5c1ac5638d8
raf <rafs@ramtracking.com>
2025-02-23 18:34:55 +0000

    chore: started writing update function

--- backend/src/main/java/com/raftracking/backend/PlaceController.java
+++ backend/src/main/java/com/raftracking/backend/PlaceController.java
@@ -37,6 +37,22 @@ public class PlaceController {
     }
 
     /**
+     * PUT /places/{id} Access: User must be authenticated Description: Updates an existing place
+     * with the specified id if it belongs to the authenticated user. Request Body: Place object
+     */
+    @PutMapping("/{id}")
+    public Place updatePlace(
+            @PathVariable Long id, @RequestBody Place updatedPlace, Authentication authentication) {
+        Place place = placeRepository.findById(id).orElseThrow();
+        if (!place.getUserId().equals(authentication.getName())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+        updatedPlace.setId(id);
+        updatedPlace.setUserId(place.getUserId());
+        return placeRepository.save(updatedPlace);
+    }
+
+    /**
      * DELETE /places/{id} Access: User must be authenticated Description: Deletes a place with the
      * specified id if it belongs to the authenticated user. Path Variable: id (Long)
      */
