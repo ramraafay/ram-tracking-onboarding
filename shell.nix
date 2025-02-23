a97a400d2c599b5f79b2a358c605cef848060bba
raf <rafs@ramtracking.com>
2025-02-23 15:52:02 +0000

    update: simplified backend & frontend & test command

--- shell.nix
+++ shell.nix
@@ -13,9 +13,24 @@ pkgs.mkShell {
     pnpm
     nodejs_22
     nodePackages.prettier
+
+    (pkgs.writeScriptBin "backend" ''
+      #!${pkgs.stdenv.shell}
+      cd $PROJECT_HOME/backend && mvn spring-boot:run
+    '')
+    (pkgs.writeScriptBin "frontend" ''
+      #!${pkgs.stdenv.shell}
+      cd $PROJECT_HOME/frontend && pnpm dev
+    '')
+    (pkgs.writeScriptBin "test" ''
+      #!${pkgs.stdenv.shell}
+      cd $PROJECT_HOME/backend && mvn test
+      cd $PROJECT_HOME/frontend && pnpm test
+    '')
   ];
 
   shellHook = ''
+    export PROJECT_HOME=$(pwd)
     export JDTLS_JVM_ARGS="-javaagent:$HOME/.m2/repository/org/projectlombok/lombok/1.18.36/lombok-1.18.36.jar"
     echo
     echo -e "\033[1;35mwelcome to the development shell!\033[0m"
